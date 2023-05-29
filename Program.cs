0c374e0925fe23e4c9d789630416cce6ad775437
Aleksey <nighttom@mail.ru>
2023-05-29 20:23:01 +0700

    zadacha 34

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿int[] RandomArray(int size)
+{
+    int[] newArray = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(99, 999);
+    }
+    return newArray;
+}
+void PrintArray(int[] newArray)
+{
+    for(int i = 0; i < newArray.Length; i++)
+    {
+        Console.Write(newArray[i] + " ");
+    }
+}
+int kol(int[] RandomArray)
+{
+    int kol = 0;
+    for (int i = 0; i < RandomArray.Length; i++)
+    {
+        if (RandomArray[i] % 2 == 0)
+        kol = kol + 1;
+    }
+    return kol;
+}
+Console.WriteLine("Input array size");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray = RandomArray(size);
+
+PrintArray(myArray);
+Console.WriteLine($"\nThe number of even numbers in the array: -> {kol(RandomArray(size))}");
